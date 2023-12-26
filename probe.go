94eb1db5e0b3abcca43cc45296ce048bf0c0f4c9
Thomas Kjær <thomas@thomaskjaer.com>
2023-12-27 00:07:38 +0100

    Add probe run function

--- probe.go
+++ probe.go
@@ -4,8 +4,11 @@ import (
 	"net"
 	"net/netip"
 	"os"
+	"time"
 
+	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 	"github.com/sirupsen/logrus"
 )
 
@@ -21,7 +24,7 @@ type probe struct {
 	srcMAC          net.HardwareAddr
 	dstMAC          net.HardwareAddr
 	interProbeDelay uint
-	numProbes       uint
+	numProbes       uint // TODO: change to uint32
 	maxTTL          uint8
 }
 
@@ -72,7 +75,8 @@ func (p *probe) init() {
 			log.Fatal(err)
 		}
 		p.srcIP = srcIP
-		p.srcIface = net.Interface{Index: 1, Name: "en0"}
+		// TODO: implement interface lookup
+		p.srcIface = net.Interface{Index: 1, Name: Args.sourceInterface}
 	} else {
 		// TODO: implement lookupSrc()
 		log.Fatal("source IP not specified, which is required for now")
@@ -96,6 +100,121 @@ func (p *probe) init() {
 	p.maxTTL = uint8(Args.maxTTL)
 }
 
+// Encode the probe number and TTL into a single value.
+//
+// To keep the sequence number small enough to fit into the UDP frame
+// length, TTL is multiplied by 20 and the probe number added.
+//
+// This leaves room for 20 probes (0-19) and a max TTL of 60 while
+// keeping the UDP payload length well under 1500 bytes.
+//
+// TTL + probe + UDP header = 60*20 + 19 + 8 == 1227
+func encodeSeq(ttl uint8, probeNum uint) (seq uint32) {
+	return uint32(ttl)*20 + uint32(probeNum)
+}
+
+// Decode the sequence and return TTL and probe number.
+func decodeSeq(seq uint32) (ttl uint8, probeNum uint) {
+	return uint8(seq / 20), uint(seq % 20)
+}
+
 func (p *probe) run() {
 	log.Info("Starting probe")
+
+	buf := gopacket.NewSerializeBuffer()
+
+	opts := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+
+	eth := layers.Ethernet{
+		SrcMAC:       p.srcMAC,
+		DstMAC:       p.dstMAC,
+		EthernetType: p.etherType,
+	}
+
+	handle, err := pcap.OpenLive(p.srcIface.Name, 65536, true, pcap.BlockForever)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	for i := uint(0); i < p.numProbes; i++ {
+		// Probe number is 0-indexed, so we add 1 before logging
+		log.Debugf("Sending probe %d", i+1)
+		for j := uint8(1); j <= p.maxTTL; j++ {
+			log.Debugf("TTL: %d", j)
+
+			switch p.inet {
+			case layers.IPProtocolIPv4:
+				ip := layers.IPv4{
+					Version:  4,
+					TTL:      j,
+					Protocol: p.proto,
+					SrcIP:    p.srcIP.AsSlice(),
+					DstIP:    p.dstIP.AsSlice(),
+					Flags:    layers.IPv4DontFragment,
+				}
+				switch p.proto {
+				case layers.IPProtocolTCP:
+					tcp := layers.TCP{
+						Seq:     encodeSeq(j, i),
+						SrcPort: layers.TCPPort(p.srcPort),
+						DstPort: layers.TCPPort(p.dstPort),
+						SYN:     true,
+						Window:  65535,
+					}
+					tcp.SetNetworkLayerForChecksum(&ip)
+					gopacket.SerializeLayers(buf, opts, &eth, &ip, &tcp)
+					handle.WritePacketData(buf.Bytes())
+				case layers.IPProtocolUDP:
+					udp := layers.UDP{
+						SrcPort: layers.UDPPort(p.srcPort),
+						DstPort: layers.UDPPort(p.dstPort),
+						Length:  uint16(8 + encodeSeq(j, i)),
+					}
+					udp.SetNetworkLayerForChecksum(&ip)
+					gopacket.SerializeLayers(buf, opts, &eth, &ip, &udp)
+					handle.WritePacketData(buf.Bytes())
+				}
+			case layers.IPProtocolIPv6:
+				ip := layers.IPv6{
+					Version:    6,
+					HopLimit:   j,
+					NextHeader: p.proto,
+					SrcIP:      p.srcIP.AsSlice(),
+					DstIP:      p.dstIP.AsSlice(),
+				}
+				switch p.proto {
+				case layers.IPProtocolTCP:
+					tcp := layers.TCP{
+						Seq:     encodeSeq(j, i),
+						SrcPort: layers.TCPPort(p.srcPort),
+						DstPort: layers.TCPPort(p.dstPort),
+						SYN:     true,
+						Window:  65535,
+					}
+					tcp.SetNetworkLayerForChecksum(&ip)
+					gopacket.SerializeLayers(buf, opts, &eth, &ip, &tcp)
+					handle.WritePacketData(buf.Bytes())
+				case layers.IPProtocolUDP:
+					udp := layers.UDP{
+						SrcPort: layers.UDPPort(p.srcPort),
+						DstPort: layers.UDPPort(p.dstPort),
+						Length:  uint16(8 + encodeSeq(j, i)),
+					}
+					udp.SetNetworkLayerForChecksum(&ip)
+					gopacket.SerializeLayers(buf, opts, &eth, &ip, &udp)
+					handle.WritePacketData(buf.Bytes())
+				}
+			}
+		}
+		// TODO: should this be a timer "since last probe run was started" instead?
+		time.Sleep(time.Duration(p.interProbeDelay) * time.Second)
+	}
+	// TODO: Remove this once we have timing properly implemented
+	// and possibly a return channel that tells us when the final
+	// SYNACK has been received.
+	time.Sleep(time.Duration(p.interProbeDelay) * time.Second)
 }
