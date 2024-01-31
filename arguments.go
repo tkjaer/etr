31357fd32608197e557f7fb627860238d6644014
Thomas Kjær <thomas@thomaskjaer.com>
2024-01-31 07:59:38 +0100

    Increase timers

--- arguments.go
+++ arguments.go
@@ -37,8 +37,8 @@ func getArgs() error {
 	flag.UintVar(&Args.sourcePort, "s", 65000, "source port")
 	flag.UintVar(&Args.numProbes, "c", 10, "probe count")
 	flag.UintVar(&Args.maxTTL, "m", 30, "maximum TTL")
-	flag.DurationVar(&Args.interTTLDelay, "h", 20*time.Millisecond, "inter-TTL delay (delay between each TTL or hop for a probe)")
-	flag.DurationVar(&Args.interProbeDelay, "d", 1*time.Second, "inter-probe delay (delay between each probe)")
+	flag.DurationVar(&Args.interTTLDelay, "h", 50*time.Millisecond, "inter-TTL delay (delay between each TTL or hop for a probe)")
+	flag.DurationVar(&Args.interProbeDelay, "d", 2*time.Second, "inter-probe delay (delay between each probe)")
 	flag.DurationVar(&Args.timeout, "t", 1*time.Second, "timeout")
 	// temporary flag based MAC assignment
 	// TODO: remove this once the lookup function is implemented
