ac319d2de814c5a48962c607a9a20bff961ac8b5
Thomas Kjær <thomas@thomaskjaer.com>
2025-04-27 09:21:09 +0200

    Move transmit to its own file

--- probe.go
+++ probe.go
@@ -1,31 +1,23 @@
 package main
 
 import (
-	"context"
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"net"
 	"net/netip"
-	"os"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
 
-	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
-	"github.com/jellydator/ttlcache/v3"
-	"github.com/tkjaer/etr/pkg/arp"
 	"github.com/tkjaer/etr/pkg/route"
-	"golang.org/x/term"
 )
 
 type probe struct {
-	destination string
-	route       route.Route
-	/* clean stuff up below */
+	destination     string
+	route           route.Route
 	proto           layers.IPProtocol
 	inet            layers.IPProtocol
 	etherType       layers.EthernetType
@@ -219,125 +211,4 @@ func (p *probe) output(outputChan chan outputMsg, stop chan struct{}, wg *sync.W
 		fmt.Printf("%2v. %-15v %3v - %v (%v)\n", msg.ttl, msg.ip, msg.loss, msg.probeNum, msg.flag)
 	}
 	// fmt.Printf("%3v. %-15v %3v - %v (%v)\n", ttl, ip, timestamp.Sub(start), probeNum, flag)
-}
-
-func (p *probe) sendProbes(handle *pcap.Handle, sentChan chan sentMsg, responseReceivedChan chan uint, stop chan struct{}, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	buf := gopacket.NewSerializeBuffer()
-
-	opts := gopacket.SerializeOptions{
-		FixLengths:       true,
-		ComputeChecksums: true,
-	}
-	eth := layers.Ethernet{
-		SrcMAC:       p.route.Interface.HardwareAddr,
-		DstMAC:       p.dstMAC,
-		EthernetType: p.etherType,
-	}
-
-	// TODO: Maybe keep a single start time and use the offset from that for all probes?
-	var lastProbeStart time.Time
-
-	for n := uint(0); n < p.numProbes; n++ {
-		// Probe number is 0-indexed, so we add 1 before logging
-		log.Debugf("Sending probe %d", n)
-
-		lastProbeStart = time.Now()
-
-		t := uint8(0)
-
-	TTLLoop:
-		for t < p.maxTTL {
-
-			select {
-			// Stop sending TTL increments if we've received a response for this
-			// probe.
-			case response := <-responseReceivedChan:
-				if response == n {
-					break TTLLoop
-				} else {
-					log.Debugf("Received response for probe %d, but expected %d", response, n)
-				}
-			default:
-				t++
-				switch p.inet {
-				case layers.IPProtocolIPv4:
-					ip := layers.IPv4{
-						Version:  4,
-						TTL:      t,
-						Protocol: p.proto,
-						SrcIP:    p.route.Source.AsSlice(),
-						DstIP:    p.route.Destination.AsSlice(),
-						Flags:    layers.IPv4DontFragment,
-					}
-					switch p.proto {
-					case layers.IPProtocolTCP:
-						tcp := layers.TCP{
-							Seq:     encodeSeq(t, n),
-							SrcPort: layers.TCPPort(p.srcPort),
-							DstPort: layers.TCPPort(p.dstPort),
-							SYN:     true,
-							Window:  65535,
-						}
-						tcp.SetNetworkLayerForChecksum(&ip)
-						gopacket.SerializeLayers(buf, opts, &eth, &ip, &tcp)
-						handle.WritePacketData(buf.Bytes())
-					case layers.IPProtocolUDP:
-						udp := layers.UDP{
-							SrcPort: layers.UDPPort(p.srcPort),
-							DstPort: layers.UDPPort(p.dstPort),
-							Length:  uint16(8 + encodeSeq(t, n)),
-						}
-						udp.SetNetworkLayerForChecksum(&ip)
-						gopacket.SerializeLayers(buf, opts, &eth, &ip, &udp)
-						handle.WritePacketData(buf.Bytes())
-					}
-				case layers.IPProtocolIPv6:
-					ip := layers.IPv6{
-						Version:    6,
-						HopLimit:   t,
-						NextHeader: p.proto,
-						SrcIP:      p.route.Source.AsSlice(),
-						DstIP:      p.route.Destination.AsSlice(),
-					}
-					switch p.proto {
-					case layers.IPProtocolTCP:
-						tcp := layers.TCP{
-							Seq:     encodeSeq(t, n),
-							SrcPort: layers.TCPPort(p.srcPort),
-							DstPort: layers.TCPPort(p.dstPort),
-							SYN:     true,
-							Window:  65535,
-						}
-						tcp.SetNetworkLayerForChecksum(&ip)
-						gopacket.SerializeLayers(buf, opts, &eth, &ip, &tcp)
-						handle.WritePacketData(buf.Bytes())
-					case layers.IPProtocolUDP:
-						udp := layers.UDP{
-							SrcPort: layers.UDPPort(p.srcPort),
-							DstPort: layers.UDPPort(p.dstPort),
-							Length:  uint16(8 + encodeSeq(t, n)),
-						}
-						udp.SetNetworkLayerForChecksum(&ip)
-						gopacket.SerializeLayers(buf, opts, &eth, &ip, &udp)
-						handle.WritePacketData(buf.Bytes())
-					}
-				}
-				sentChan <- sentMsg{n, t, time.Now()}
-				time.Sleep(p.interTTLDelay)
-			}
-
-		}
-		// Sleep for interProbeDelay if we haven't already spent that much time
-		// sending the probe.
-		if time.Since(lastProbeStart) < p.interProbeDelay {
-			time.Sleep(p.interProbeDelay - time.Since(lastProbeStart))
-		}
-		fmt.Println("")
-	}
-	// Sleep for probe timeout to ensure we don't miss any late responses.
-	time.Sleep(p.timeout)
-	log.Debug("Finished sending probes")
-	close(stop)
 }
