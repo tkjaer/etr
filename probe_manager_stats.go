18f200112bd7bf812780aac9976f475a75c49951
Thomas Kjær <thomas@thomaskjaer.com>
2025-10-11 18:56:38 +0200

    Clean up pm stats access and fix types in output interface

--- probe_manager_stats.go
+++ probe_manager_stats.go
@@ -28,16 +28,17 @@ type TTLCacheValue struct {
 
 // Holds stats for a single probe instance
 type Probe struct {
-	ProbeID  uint16
-	Hops     map[uint8]*HopStats // TTL -> hop stats
-	Sent     uint
-	Received uint
+	ProbeID uint16
+	Hops    map[uint8]*HopStats // TTL -> hop stats
 }
 
 // Holds stats for a single hop (TTL)
 type HopStats struct {
 	IPs       map[string]*HopIPStats // IP string -> stats
 	CurrentIP string                 // Most recent IP seen at this hop
+	Received  uint                   // Number of probes received at this TTL
+	Lost      uint                   // Number of probes lost at this TTL
+	LossPct   float64                // Percentage loss at this TTL
 }
 
 // Holds stats for a single IP at a given hop
@@ -45,19 +46,20 @@ type HopIPStats struct {
 	Min, Max   int64   // RTT in microseconds
 	Avg        int64   // RTT in microseconds
 	Stdev      float64 // RTT standard deviation in microseconds
-	Loss       uint    // Number of timeouts/losses
+	Lost       uint    // Number of timeouts/losses
+	LossPct    float64 // Percentage loss
 	Responses  uint    // Number of responses
 	Sum        int64   // Sum of RTTs for calculating average
 	SumSquares int64   // Sum of squares for stddev calculation
 }
 
 func (pm *ProbeManager) statsProcessor() {
-	stats := ProbeStats{
+	pm.stats = ProbeStats{
 		Probes:   make(map[uint16]*Probe),
 		Mutex:    sync.RWMutex{},
 		TTLCache: ttlcache.New[TTLCacheKey, TTLCacheValue](ttlcache.WithTTL[TTLCacheKey, TTLCacheValue](pm.probeConfig.timeout)),
 	}
-	stats.TTLCache.OnEviction(func(ctx context.Context, reason ttlcache.EvictionReason, item *ttlcache.Item[TTLCacheKey, TTLCacheValue]) {
+	pm.stats.TTLCache.OnEviction(func(ctx context.Context, reason ttlcache.EvictionReason, item *ttlcache.Item[TTLCacheKey, TTLCacheValue]) {
 		if reason == ttlcache.EvictionReasonExpired {
 			pm.statsChan <- ProbeEvent{
 				ProbeID:   item.Key().ProbeID,
@@ -69,8 +71,8 @@ func (pm *ProbeManager) statsProcessor() {
 			}
 		}
 	})
-	go stats.TTLCache.Start()
-	defer stats.TTLCache.Stop()
+	go pm.stats.TTLCache.Start()
+	defer pm.stats.TTLCache.Stop()
 
 	for event := range pm.statsChan {
 		// Update internal stats (maps, counters, etc.)
@@ -78,11 +80,11 @@ func (pm *ProbeManager) statsProcessor() {
 		switch event.EventType {
 		case "sent":
 			if data, ok := event.Data.(*ProbeEventDataSent); ok {
-				pm.updateSentStats(&stats, event.ProbeID, data)
+				pm.updateSentStats(event.ProbeID, data)
 			}
 		case "received":
 			if data, ok := event.Data.(*ProbeEventDataReceived); ok {
-				pm.updateReceivedStats(&stats, event.ProbeID, data)
+				pm.updateReceivedStats(event.ProbeID, data)
 			}
 		case "timeout":
 			// if data, ok := event.Data.(*ProbeEventDataTimeout); ok {
@@ -94,33 +96,29 @@ func (pm *ProbeManager) statsProcessor() {
 	}
 }
 
-func (pm *ProbeManager) updateSentStats(stats *ProbeStats, probeID uint16, data *ProbeEventDataSent) {
-	stats.Mutex.Lock()
-	defer stats.Mutex.Unlock()
+func (pm *ProbeManager) updateSentStats(probeID uint16, data *ProbeEventDataSent) {
+	pm.stats.Mutex.Lock()
+	defer pm.stats.Mutex.Unlock()
 
 	// Get or create ProbeStats entry
-	probeStats, exists := stats.Probes[probeID]
+	probeStats, exists := pm.stats.Probes[probeID]
 	if !exists {
 		probeStats = &Probe{
 			ProbeID: probeID,
-			Sent:    0,
-			// FIXME: Do we actually want a received counter here?
-			Received: 0,
-			Hops:     make(map[uint8]*HopStats),
+			Hops:    make(map[uint8]*HopStats),
 		}
-		stats.Probes[probeID] = probeStats
+		pm.stats.Probes[probeID] = probeStats
 	}
 
-	stats.TTLCache.Set(TTLCacheKey{ProbeID: probeID, TTL: data.TTL}, TTLCacheValue{SentTime: data.Timestamp}, pm.probeConfig.timeout)
-	probeStats.Sent++
+	pm.stats.TTLCache.Set(TTLCacheKey{ProbeID: probeID, TTL: data.TTL}, TTLCacheValue{SentTime: data.Timestamp}, pm.probeConfig.timeout)
 }
 
-func (pm *ProbeManager) updateReceivedStats(stats *ProbeStats, probeID uint16, data *ProbeEventDataReceived) {
-	stats.Mutex.Lock()
-	defer stats.Mutex.Unlock()
+func (pm *ProbeManager) updateReceivedStats(probeID uint16, data *ProbeEventDataReceived) {
+	pm.stats.Mutex.Lock()
+	defer pm.stats.Mutex.Unlock()
 
 	// Check if we have stats for this probeID
-	probeStats, exists := stats.Probes[probeID]
+	probeStats, exists := pm.stats.Probes[probeID]
 	if !exists {
 		// This should not happen; received a probe for unknown probeID
 		log.Debugf("Received probe for unknown probeID %d", probeID)
@@ -134,19 +132,13 @@ func (pm *ProbeManager) updateReceivedStats(stats *ProbeStats, probeID uint16, d
 		return
 	}
 
+	hopStats.Received++
+	hopStats.LossPct = calculateLossPct(hopStats.Lost, hopStats.Received)
+
 	// Get or create HopIPStats
 	ipStats, exists := hopStats.IPs[data.IP]
 	if !exists {
-		ipStats = &HopIPStats{
-			Min:        0,
-			Max:        0,
-			Avg:        0,
-			Stdev:      0,
-			Loss:       0,
-			Responses:  0,
-			Sum:        0,
-			SumSquares: 0,
-		}
+		ipStats = &HopIPStats{}
 		hopStats.IPs[data.IP] = ipStats
 	}
 
@@ -156,7 +148,7 @@ func (pm *ProbeManager) updateReceivedStats(stats *ProbeStats, probeID uint16, d
 	// Check and remove from TTL cache
 	cacheKey := TTLCacheKey{ProbeID: probeID, TTL: data.TTL}
 	sentTime := time.Time{}
-	if cacheEntry, present := stats.TTLCache.GetAndDelete(cacheKey); !present {
+	if cacheEntry, present := pm.stats.TTLCache.GetAndDelete(cacheKey); !present {
 		sentTime = cacheEntry.Value().SentTime
 	} else {
 		log.Debugf("Received probe for TTL %d on probeID %d that has already expired", data.TTL, probeID)
