0701b794b021c21251df25198b9ce909d5462c6e
Thomas Kjær <thomas@thomaskjaer.com>
2025-11-01 13:21:07 +0100

    Refactor ndp pkg and add tests

--- pkg/ndp/ndp_linux.go
+++ pkg/ndp/ndp_linux.go
@@ -10,21 +10,30 @@ import (
 	"github.com/jsimonetti/rtnetlink/rtnl"
 )
 
-// CheckNeighbourTable checks if IP is in the kernel Neighbour table for the provided interface
-func checkNeighbourTable(ip net.IP, iface *net.Interface) (net.HardwareAddr, error) {
+// getNeighbourTable fetches neighbors. Variable for mocking in tests.
+var getNeighbourTable = func(iface *net.Interface) ([]*rtnl.Neigh, error) {
 	c, err := rtnl.Dial(nil)
 	if err != nil {
 		return nil, err
 	}
 	defer c.Close()
+	return c.Neighbours(iface, 0)
+}
 
-	r, err := c.Neighbours(iface, 0)
+// isNeighbourMatch checks if a neighbor entry matches the target IP
+func isNeighbourMatch(n *rtnl.Neigh, ip net.IP) bool {
+	return n.IP.Equal(ip)
+}
+
+// CheckNeighbourTable checks if IP is in the kernel Neighbour table for the provided interface
+func checkNeighbourTable(ip net.IP, iface *net.Interface) (net.HardwareAddr, error) {
+	neighbors, err := getNeighbourTable(iface)
 	if err != nil {
 		return nil, err
 	}
 
-	for _, n := range r {
-		if n.IP.Equal(ip) {
+	for _, n := range neighbors {
+		if isNeighbourMatch(n, ip) {
 			return n.HwAddr, nil
 		}
 	}
