4a353974480324b88f4b7cb0a699e2684f5158b7
Thomas Kjær <thomas@thomaskjaer.com>
2026-03-23 10:56:16 +0100

    Add JSON discover support and improve TUI (#105)
    * Unlimited discover range should be unlimited
    
    * Update discover json support
    
    * Try to use terminal height better
    
    * Rewrite path visualization example to support discovery results

--- internal/config/arguments.go
+++ internal/config/arguments.go
@@ -97,7 +97,7 @@ func ParseArgs() (Args, error) {
 	flag.BoolVarP(&args.ForceIPv4, "ipv4", "4", false, "Force IPv4")
 	flag.BoolVarP(&args.ForceIPv6, "ipv6", "6", false, "Force IPv6")
 	flag.BoolVarP(&args.NoResolve, "no-resolve", "n", false, "Do not resolve IP addresses to hostnames")
-	flag.BoolVarP(&args.LookupASN, "asn", "a", false, "Lookup ASN for hop and destination IPs via DNS (Team Cymru, cached) with WHOIS fallback")
+	flag.BoolVarP(&args.LookupASN, "asn", "a", false, "Lookup ASN for hop and destination IPs")
 	flag.BoolVarP(&args.PrintBPFFilter, "print-bpf", "B", false, "Print tcpdump-compatible BPF filter and exit")
 	flag.UintVarP(&args.DestinationPort, "dest-port", "p", 0, "Destination port (default: 443 for TCP, 33434 for UDP)")
 	flag.UintVarP(&args.SourcePort, "source-port", "s", 50000, "Base source port")
